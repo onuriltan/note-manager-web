89ae044553f0eeaa0b2707ef850805e3a459e251
onuriltan <onur.iltan@gmail.com>
2019-01-18 09:58:35 +0300

    add sketchy bootstrap design

---
+++ server/public/precache-manifest.82874c72b5f25d797628de37b4562277.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3c663d600f311e309bd",
+    "url": "/css/app.998aaa9a.css"
+  },
+  {
+    "revision": "f3c663d600f311e309bd",
+    "url": "/js/app.54584eb4.js"
+  },
+  {
+    "revision": "4cb649e518c9cfb9465c",
+    "url": "/css/chunk-3f99eee6.e7eac981.css"
+  },
+  {
+    "revision": "4cb649e518c9cfb9465c",
+    "url": "/js/chunk-3f99eee6.8a446bba.js"
+  },
+  {
+    "revision": "3b152bdb6af1824d2ebe",
+    "url": "/css/chunk-vendors.0925668a.css"
+  },
+  {
+    "revision": "3b152bdb6af1824d2ebe",
+    "url": "/js/chunk-vendors.add6bd57.js"
+  },
+  {
+    "revision": "cfca4bf3063610e35b72824e11256eee",
+    "url": "/img/takingnotes.cfca4bf3.jpg"
+  },
+  {
+    "revision": "b98cf470313470682ae0a72b3d41f89c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
