b4da02f20a5228f011b592820592be51f873c588
onuriltan <onur.iltan@gmail.com>
2019-02-01 11:31:23 +0300

    mail operations update

--- server/config/MailOperations.js
+++ server/config/MailOperations.js
@@ -1,25 +1,38 @@
 const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.MAIL_USERNAME,
         pass: process.env.MAIL_PASSWORD
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
-async function sendConfirmationMail(to, confirmationToken) {
+
+function sendConfirmationMail(to, confirmationToken) {
     let mailOptions = {
-        from: '"Note Manager 👻" <'+process.env.MAIL+'>', // sender address
+        from: '"Note Manager 👻" <' + process.env.MAIL + '>', // sender address
         to: to, // list of receivers
         subject: "Welcome to note manager ✔", // Subject line
-        text: "Please confirm your account with this token = "+confirmationToken, // plain text body
-        html: "<a href="+process.env.CONFIRM_EMAIL_URL+"/"+confirmationToken+">Click to activate your account</a>" // html body
+        text: "Please confirm your account with this token = " + confirmationToken, // plain text body
+        html: "<a href=" + process.env.CONFIRM_EMAIL_URL + "/" + confirmationToken + ">Click to activate your account</a>" // html body
     };
     // send mail with defined transport object
-    let info = await transporter.sendMail(mailOptions)
-    // console.log("Message sent: %s", info.messageId);
-    // Preview only available when sending through an Ethereal account
-    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    return new Promise(function(resolve, reject) {
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(info);
+            }
+        });
+    });
+
 }
+
 module.exports.sendConfirmationMail = sendConfirmationMail;
