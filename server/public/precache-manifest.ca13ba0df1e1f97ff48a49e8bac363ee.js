13d391ddf673d64f7c23295ecb1cb0f6f00ebb93
onuriltan <onur.iltan@gmail.com>
2019-02-14 21:53:55 +0300

    implement google auth

---
+++ server/public/precache-manifest.ca13ba0df1e1f97ff48a49e8bac363ee.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "8149cf3099527266e8e5",
+    "url": "/css/app.0d6e3055.css"
+  },
+  {
+    "revision": "8149cf3099527266e8e5",
+    "url": "/js/app.3d94c5f5.js"
+  },
+  {
+    "revision": "4d59bcc95533233e9da9",
+    "url": "/css/chunk-0027d6e3.57391644.css"
+  },
+  {
+    "revision": "4d59bcc95533233e9da9",
+    "url": "/js/chunk-0027d6e3.c5ff285b.js"
+  },
+  {
+    "revision": "0ef182e890e0c113b6bc",
+    "url": "/css/chunk-14028e07.f181453d.css"
+  },
+  {
+    "revision": "0ef182e890e0c113b6bc",
+    "url": "/js/chunk-14028e07.1902c0df.js"
+  },
+  {
+    "revision": "7b2088b1f8479c2a727c",
+    "url": "/css/chunk-1d6aee91.cfa4b34b.css"
+  },
+  {
+    "revision": "7b2088b1f8479c2a727c",
+    "url": "/js/chunk-1d6aee91.e44b0e1c.js"
+  },
+  {
+    "revision": "4bfa4686c09dd2c70261",
+    "url": "/css/chunk-28acc20d.a44ffebe.css"
+  },
+  {
+    "revision": "4bfa4686c09dd2c70261",
+    "url": "/js/chunk-28acc20d.0d0f9c42.js"
+  },
+  {
+    "revision": "908acb9ed1728e9226cf",
+    "url": "/js/chunk-2d237c74.6d079693.js"
+  },
+  {
+    "revision": "5d5f2fa15a9dbb5fddbf",
+    "url": "/css/chunk-56fe4638.c5841964.css"
+  },
+  {
+    "revision": "5d5f2fa15a9dbb5fddbf",
+    "url": "/js/chunk-56fe4638.e2bac479.js"
+  },
+  {
+    "revision": "7242f5ef90511cba9c71",
+    "url": "/css/chunk-vendors.0925668a.css"
+  },
+  {
+    "revision": "7242f5ef90511cba9c71",
+    "url": "/js/chunk-vendors.0c7ea100.js"
+  },
+  {
+    "revision": "747c5c40f60d98e9ed201a92adf08e7f",
+    "url": "/img/empty-paper.747c5c40.png"
+  },
+  {
+    "revision": "1ae7649ebe9463b0c0912034dc1953b5",
+    "url": "/media/nyan-cat-short.1ae7649e.mp3"
+  },
+  {
+    "revision": "cfca4bf3063610e35b72824e11256eee",
+    "url": "/img/takingnotes.cfca4bf3.jpg"
+  },
+  {
+    "revision": "d9da77312f704cf6896814a0d5cb76c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34a705272b8a0c027ee3994b99a0d089.js"
+  "/precache-manifest.ca13ba0df1e1f97ff48a49e8bac363ee.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "client"});
