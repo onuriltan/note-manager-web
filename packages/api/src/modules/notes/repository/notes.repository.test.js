3298f42f831b5b6714b56479f8ca80fe9e4926e1
onuriltan <onur.iltan@gmail.com>
2020-06-20 10:09:40 +0200

    add more tests for notes.repository

--- packages/api/src/modules/notes/repository/notes.repository.test.js
+++ packages/api/src/modules/notes/repository/notes.repository.test.js
@@ -18,3 +18,68 @@ describe(`${noteRepository.findNotes.name} Repository`, () => {
     expect(paginate).toHaveBeenCalledWith({ email }, { ...options, lean: true })
   })
 })
+
+describe(`${noteRepository.findNotesBetweenDatesandKeyword.name} Repository`, () => {
+  const fromDate = new Date(1995, 11, 17)
+  const toDate = new Date(1995, 11, 18)
+  const keyword = 'keyword'
+  const email = 'onur@iltan.com'
+  const options = {}
+  const paginate = jest.spyOn(noteEntity, 'paginate').mockReturnValue([])
+
+  it(`should return the result of NoteEntity.paginate`, async () => {
+    // Arrange
+    const query = {
+      email: email,
+      createdAt: {
+        $gte: fromDate,
+        $lt: toDate,
+      },
+      text: { $regex: new RegExp(`${keyword}`, 'i') },
+    }
+
+    // Act
+    await noteRepository.findNotesBetweenDatesandKeyword(
+      fromDate,
+      toDate,
+      keyword,
+      email,
+      options
+    )
+
+    // Assert
+    expect(paginate).toHaveBeenCalledWith(query, {
+      ...options,
+      lean: true,
+      sort: { date: -1 },
+    })
+  })
+
+  it(`should not add text to query if no keyword is coming`, async () => {
+    // Arrange
+    const keyword = ''
+    const query = {
+      email: email,
+      createdAt: {
+        $gte: fromDate,
+        $lt: toDate,
+      },
+    }
+
+    // Act
+    await noteRepository.findNotesBetweenDatesandKeyword(
+      fromDate,
+      toDate,
+      keyword,
+      email,
+      options
+    )
+
+    // Assert
+    expect(paginate).toHaveBeenCalledWith(query, {
+      ...options,
+      lean: true,
+      sort: { date: -1 },
+    })
+  })
+})
