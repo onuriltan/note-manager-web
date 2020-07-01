c9cf64d673e26206f72c0bb4acf6792b38004d79
onuriltan <onur.iltan@gmail.com>
2020-07-01 14:26:05 +0200

    fix: delete import of the deleted library

--- packages/web/src/store/index.js
+++ packages/web/src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import createPersistedState from 'vuex-persistedstate';
 import VuexPersistence from 'vuex-persist';
 
 import auth from './modules/auth.store';
