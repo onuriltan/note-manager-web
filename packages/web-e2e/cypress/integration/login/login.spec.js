6473c19ffa334e3f3d9dfb2808bec3f3f3fce0a1
onuriltan <onur.iltan@gmail.com>
2020-11-15 17:57:07 +0300

    feat: write first test for login

--- packages/web-e2e/cypress/integration/login/login.spec.js
+++ packages/web-e2e/cypress/integration/login/login.spec.js
@@ -1,5 +1,14 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
+describe('Login', () => {
+    it('Logs in with the example account', () => {
+        cy.visit(Cypress.env('CLIENT_URL'))
+
+        cy.get('#email')
+        .type('vehbi.iltan@gmail.com')
+        cy.get('#password')
+        .type('123456')
+        cy.get('#login-button').click()
+
+        cy.wait(1000)
+        cy.url().should('include', '/dashboard')
     })
   })
