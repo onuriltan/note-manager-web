e0e3aa973e99f2f64f12638d3534f87ec73819ed
Onur Iltan <onur.iltan@gmail.com>
2020-12-12 13:36:55 +0300

    feature: password strength (#89)
    * feat: update server file for better error handling and logging
    
    * feat: add password validation to express-validator
    
    and some small changes
    
    * feat: add common package and move password strength checker there
    
    * feat: remove common package
    
    * feat: linting
    
    * feat: WIP add password validation and strength checker
    
    * fix: passwordStrength value

--- packages/web/src/helpers/validators.js
+++ packages/web/src/helpers/validators.js
@@ -3,7 +3,6 @@ export function validateRegister(email, password, password2) {
     email: '',
     password: '',
     password2: '',
-    errorCount: 0,
   };
 
   if (!email) {
@@ -18,19 +17,14 @@ export function validateRegister(email, password, password2) {
     fieldErrors.password = 'Password required.';
   } else if (password.length < 10 || password.length > 20) {
     fieldErrors.password = 'Password length should between 10 and 20.';
-    errorCount++;
   } else if (!password.match(/[a-z]/)) {
     fieldErrors.password = 'Password should have at least one lowercase character.';
-    errorCount++;
   } else if (!password.match(/[A-Z]/)) {
     fieldErrors.password = 'Password should have at least one uppercase character.';
-    errorCount++;
   } else if (!password.match(/[0-9]/)) {
     fieldErrors.password = 'Password should have at least one uppercase character.';
-    errorCount++;
   } else if (!password.match(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/)) {
     fieldErrors.password = 'Password should have at least one special character.';
-    errorCount++;
   }
 
   if (!password2) {
@@ -51,7 +45,6 @@ export function validateRegister(email, password, password2) {
   } else {
     fieldErrors.password = '';
     fieldErrors.password2 = '';
-    errorCount = 0;
   }
 
   return fieldErrors;
@@ -111,4 +104,36 @@ export function validateChangePassword(oldPassword, newPassword) {
 function validEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
+}
+
+export function measureStrengthOfPassword(password) {
+  let errorCount = 0;
+  if (!password) {
+    errorCount++;
+  }
+  if (password.length < 10) {
+    errorCount++;
+  }
+  if (!password.match(/[a-z]/)) {
+    errorCount++;
+  }
+  if (!password.match(/[A-Z]/)) {
+    errorCount++;
+  }
+  if (!password.match(/[0-9]/)) {
+    errorCount++;
+  }
+  if (!password.match(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/)) {
+    errorCount++;
+  }
+
+  if (errorCount >= 0 && errorCount <= 3) {
+    return 'medium';
+  } else if (errorCount >= 2 && errorCount <= 6) {
+    return 'weak';
+  } else if (errorCount >= 0 && errorCount <= 1) {
+    return 'strong';
+  } else {
+    return undefined;
+  }
 }
